65e65ccf9b0ec5f2cc14d7229c1a4aec64cd4e7f
erty2134 <olly.lansdown@gmail.com>
2024-07-04 20:28:11 +1000

    added help command, error: if u do a command it says command not found

--- main.py
+++ main.py
@@ -19,23 +19,50 @@ class Ccolors:
 class command:
     def __init__(self,command_name:"str"="None") -> None:
         self.cmd:"str"=command_name;
+        self.active:"bool"=False;
         self.args:"list";
         self.argc:"int";
+        commands:"list";
+        if(command_name not in commands):
+            commands+=str(command_name)
     
-    def update(self,input:"str")->None:
-        if (input[len(self.cmd)]==self.cmd):
-            pass;
+    def update(self,passInput:"str")->None:
+        if (passInput[0:len(self.cmd)]==self.cmd):
+            sys.stdout.write(f"\n{Ccolors.BOLD}{self.cmd}::{Ccolors.ENDC}")
+            sys.stdout.flush();
+            # #####
+            if(self.cmd=="help"):
+                sys.stdout.write("\ncommands:");
+                sys.stdout.write(f"{Ccolors.OKCYAN}help- shows all commands and if they are running{Ccolors.ENDC} {Ccolors.BOLD}{Ccolors.WARNING}Enabled:{help.active}{Ccolors.ENDC}\n");
+                sys.stdout.write(f"{Ccolors.OKCYAN}getTargets- show targets{Ccolors.ENDC} {Ccolors.BOLD}{Ccolors.WARNING}Enabled:{getTargets.active}{Ccolors.ENDC}\n");
+            
+            elif(self.cmd=="exit"):
+                sys.exit();
+            
+            elif(self.cmd=="clear"):
+                sys._clear_type_cache();
+            # #####
+            sys.stdout.write("\n")
+            sys.stdout.flush();
+        else:
+            sys.stderr.write(f"{Ccolors.ENDC}{Ccolors.FAIL}{Ccolors.BOLD}Warning! {passInput} is not a valid command{Ccolors.ENDC}\n");
 
 
 userInput:"str";
 
 help:"command"=command(command_name="help");
+exit:"command"=command(command_name="exit");
+clear:"command"=command(command_name="clear");
+getTargets:"command"=command(command_name="getTargets");
+
 def main(argc:int,argv:list):
     while (True):
         rawTime=datetime.datetime.now();
         time=rawTime.strftime("%H:%M:%S")
-        input(f"{Ccolors.OKBLUE}[{time}]|[{socket.gethostname()}]>>{Ccolors.ENDC}");
+        userInput=str(input(f"{Ccolors.OKBLUE}[{time}]|[{socket.gethostname()}]>>{Ccolors.ENDC}"));
 
         help.update(str(userInput));
+        exit.update(str(userInput));
+        clear.update(str(userInput));
 
 if (__name__=="__main__") : main(len(sys.argv[1:]),sys.argv[1:]);
