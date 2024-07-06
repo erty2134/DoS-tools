ae781078e1efc346cf7bd8e66d64553ea855c39b
erty2134 <olly.lansdown@gmail.com>
2024-07-06 10:48:56 +1000

    help, exit, clear, commands. no errors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #main.py
 import sys
+import os
 import socket
 import datetime
 
@@ -17,35 +18,41 @@ class Ccolors:
     UNDERLINE = "\033[4m"
 
 class command:
+    commands:"list"=["None"];
     def __init__(self,command_name:"str"="None") -> None:
         self.cmd:"str"=command_name;
         self.active:"bool"=False;
         self.args:"list";
         self.argc:"int";
-        commands:"list";
-        if(command_name not in commands):
-            commands+=str(command_name)
+        if(command_name not in self.commands):
+            self.commands.append(str(command_name));
     
     def update(self,passInput:"str")->None:
         if (passInput[0:len(self.cmd)]==self.cmd):
-            sys.stdout.write(f"\n{Ccolors.BOLD}{self.cmd}::{Ccolors.ENDC}")
+            sys.stdout.write(f"\n{Ccolors.BOLD}{self.cmd}::{Ccolors.ENDC}");
             sys.stdout.flush();
             # #####
             if(self.cmd=="help"):
-                sys.stdout.write("\ncommands:");
+                sys.stdout.write("\ncommands:\n");
                 sys.stdout.write(f"{Ccolors.OKCYAN}help- shows all commands and if they are running{Ccolors.ENDC} {Ccolors.BOLD}{Ccolors.WARNING}Enabled:{help.active}{Ccolors.ENDC}\n");
+                sys.stdout.write(f"{Ccolors.OKCYAN}exit- exits app{Ccolors.ENDC} {Ccolors.BOLD}{Ccolors.WARNING}Enabled:{help.active}{Ccolors.ENDC}\n");
+                sys.stdout.write(f"{Ccolors.OKCYAN}clear- clears console{Ccolors.ENDC} {Ccolors.BOLD}{Ccolors.WARNING}Enabled:{clear.active}{Ccolors.ENDC}\n");
                 sys.stdout.write(f"{Ccolors.OKCYAN}getTargets- show targets{Ccolors.ENDC} {Ccolors.BOLD}{Ccolors.WARNING}Enabled:{getTargets.active}{Ccolors.ENDC}\n");
+
+                sys.stdout.write(f"{self.commands}\n");
             
             elif(self.cmd=="exit"):
                 sys.exit();
             
             elif(self.cmd=="clear"):
-                sys._clear_type_cache();
+                os.system("cls"if(os.name == "nt")else"clear")
             # #####
             sys.stdout.write("\n")
             sys.stdout.flush();
         else:
-            sys.stderr.write(f"{Ccolors.ENDC}{Ccolors.FAIL}{Ccolors.BOLD}Warning! {passInput} is not a valid command{Ccolors.ENDC}\n");
+            if (self.cmd in self.commands) : pass; #
+            else : sys.stderr.write(f"{Ccolors.ENDC}{Ccolors.FAIL}{Ccolors.BOLD}Warning! '{passInput}' is not a valid command{Ccolors.ENDC}\n");
+        return None;
 
 
 userInput:"str";
